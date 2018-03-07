f2760b49ac7d5eac30bd1708eb2966ff93440a24
Rafael de Paula Herrera <herrera.rp@gmail.com>
2018-03-07 00:21:43 -0300

    on for httpd as default

--- xmrig-cpu/bin/docker-config.sh
+++ xmrig-cpu/bin/docker-config.sh
@@ -5,4 +5,4 @@ OS_FLAVOR=${OS_FLAVOR-'alpine'}
 OS_VERSION=${OS_VERSION-'3.7'}
 XMRIG_FLAVOR=${XMRIG_FLAVOR-'xmrig-cpu'}
 XMRIG_VERSION=${XMRIG_VERSION-'2.4.5'}
-DEFAULT_DOCKER_IMAGE='herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_off-libcpuid_on'
+DEFAULT_DOCKER_IMAGE='herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_on-libcpuid_on'
