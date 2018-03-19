08a19a8c02e1f5b59d2dcb542605eb94a0001ccd
Rafael de Paula Herrera <herrera.rp@gmail.com>
2018-03-18 23:19:11 -0300

    xmrig updated to v2.5.0

--- xmrig-cpu/bin/docker-config.sh
+++ xmrig-cpu/bin/docker-config.sh
@@ -4,5 +4,5 @@ AUTHOR=${AUTHOR-'herrera'}
 OS_FLAVOR=${OS_FLAVOR-'alpine'}
 OS_VERSION=${OS_VERSION-'3.7'}
 XMRIG_FLAVOR=${XMRIG_FLAVOR-'xmrig-cpu'}
-XMRIG_VERSION=${XMRIG_VERSION-'2.4.5'}
-DEFAULT_DOCKER_IMAGE='herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_on-libcpuid_on'
+XMRIG_VERSION=${XMRIG_VERSION-'2.5.0'}
+DEFAULT_DOCKER_IMAGE="${AUTHOR}/${XMRIG_FLAVOR}:${OS_FLAVOR}-${OS_VERSION}-xmrig-${XMRIG_VERSION}-aeon_on-httpd_on-libcpuid_on"
--- xmrig-cpu/bin/docker-push-and-retag.sh
+++ xmrig-cpu/bin/docker-push-and-retag.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -ex
 
-docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_off-httpd_off-libcpuid_off
-docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_off-httpd_off-libcpuid_on
-docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_off-httpd_on-libcpuid_off
-docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_off-httpd_on-libcpuid_on
-docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_off-libcpuid_on
-docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_off-libcpuid_off
-docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_on-libcpuid_on
-docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_on-libcpuid_off
+docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_off-httpd_off-libcpuid_off
+docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_off-httpd_off-libcpuid_on
+docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_off-httpd_on-libcpuid_off
+docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_off-httpd_on-libcpuid_on
+docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_on-httpd_off-libcpuid_on
+docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_on-httpd_off-libcpuid_off
+docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_on-httpd_on-libcpuid_on
+docker push herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_on-httpd_on-libcpuid_off
 
-docker tag herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_on-libcpuid_on herrera/xmrig-cpu:latest
-docker tag herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_off-libcpuid_on herrera/xmrig-cpu:latest-no-httpd
+docker tag herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_on-httpd_on-libcpuid_on herrera/xmrig-cpu:latest
+docker tag herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_on-httpd_off-libcpuid_on herrera/xmrig-cpu:latest-no-httpd
 docker push herrera/xmrig-cpu:latest
 docker push herrera/xmrig-cpu:latest-no-httpd
 
-docker tag herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_on-libcpuid_on herrera/xmrig-cpu:2.4.5
-docker tag herrera/xmrig-cpu:alpine-3.7-xmrig-2.4.5-aeon_on-httpd_off-libcpuid_on herrera/xmrig-cpu:2.4.5-no-httpd
-docker push herrera/xmrig-cpu:2.4.5
-docker push herrera/xmrig-cpu:2.4.5-no-httpd
+docker tag herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_on-httpd_on-libcpuid_on herrera/xmrig-cpu:2.5.0
+docker tag herrera/xmrig-cpu:alpine-3.7-xmrig-2.5.0-aeon_on-httpd_off-libcpuid_on herrera/xmrig-cpu:2.5.0-no-httpd
+docker push herrera/xmrig-cpu:2.5.0
+docker push herrera/xmrig-cpu:2.5.0-no-httpd
